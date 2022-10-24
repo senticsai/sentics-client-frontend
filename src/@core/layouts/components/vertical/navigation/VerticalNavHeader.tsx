3ac94a3cda49854f83464264f6d1889f884d50cc
fishuke <fishukee@gmail.com>
2022-10-24 16:35:47 +0300

    feat: detailed analytics page

--- src/@core/layouts/components/vertical/navigation/VerticalNavHeader.tsx
+++ src/@core/layouts/components/vertical/navigation/VerticalNavHeader.tsx
@@ -164,14 +164,13 @@ const VerticalNavHeader = (props: Props) => {
               userMenuLockedIcon
             )
           ) : (
-            <Box
+            <svg
               width={22}
               fill='none'
               height={22}
-              component='svg'
               viewBox='0 0 22 22'
               xmlns='http://www.w3.org/2000/svg'
-              sx={{
+              style={{
                 transform: `rotate(${svgRotationDeg()}deg)`,
                 transition: 'transform .25s ease-in-out .35s'
               }}
@@ -184,7 +183,7 @@ const VerticalNavHeader = (props: Props) => {
                 fill={svgFillDisabled()}
                 d='M15.8683 4.88844L10.6214 10.1353C10.1441 10.6126 10.1441 11.3865 10.6214 11.8638L15.8683 17.1107C16.3455 17.5879 17.1193 17.5879 17.5965 17.1107C18.0737 16.6334 18.0737 15.8597 17.5965 15.3824L14.0779 11.8638C13.6005 11.3865 13.6005 10.6126 14.0779 10.1353L17.5965 6.61667C18.0737 6.13943 18.0737 5.36568 17.5965 4.88844C17.1193 4.41121 16.3455 4.41121 15.8683 4.88844Z'
               />
-            </Box>
+            </svg>
           )}
         </IconButton>
       )}
