b0d4e756cbc73b213e5be5b5a84ba661705c9319
fishuke <fishukee@gmail.com>
2022-10-25 16:26:56 +0300

    fix: human/vehicle positions & animation

--- src/views/map/index.tsx
+++ src/views/map/index.tsx
@@ -1,25 +1,17 @@
 import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
+import {Canvas, useFrame} from '@react-three/fiber'
 import { Bounds, OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Button } from '@mui/material'
 import Map from './map'
-import * as THREE from 'three'
 
 export const MapComponent = ({ positions }: { positions: EntitiesPayload }) => {
   const [perspective, setPerspective] = React.useState<'2d' | '3d'>('2d')
   const perspectiveCamera = React.useRef<any>(null)
 
   function switchPerspective() {
-    const camera: THREE.PerspectiveCamera = perspectiveCamera.current
-
-    /**
-     * we don't need to animate if it's going from 2d to 3d.
-     * in the future we will animate it to a fixed position/rotation.
-     */
     if (perspective === '2d') {
       return setPerspective('3d')
     }
-
     setPerspective('2d')
   }
 
@@ -27,7 +19,7 @@ export const MapComponent = ({ positions }: { positions: EntitiesPayload }) => {
     <>
       <Canvas>
         <Suspense fallback={null}>
-          <PerspectiveCamera ref={perspectiveCamera} position={[0, 100, 0]} zoom={1} makeDefault />
+          <PerspectiveCamera ref={perspectiveCamera} position={[0, 0, 0]} zoom={1} makeDefault />
           <OrbitControls enabled={perspective === '3d'} />
           <Bounds clip>
             <Map entities={positions} perspective={perspective} />
--- src/views/map/map.tsx
+++ src/views/map/map.tsx
@@ -1,10 +1,10 @@
-import { DoubleSide, Shape } from 'three'
-import React from 'react'
+import {DoubleSide, Shape} from 'three'
+import React, {useEffect} from 'react'
 import Entities from './entities'
-import { getSize } from '../../helpers/getSize'
+import {getSize} from '../../helpers/getSize'
 import * as THREE from 'three'
-import { useFrame } from '@react-three/fiber'
-import { degToRad } from 'three/src/math/MathUtils'
+import {useFrame} from '@react-three/fiber'
+import {degToRad} from 'three/src/math/MathUtils'
 
 // TODO get this from api in the future
 const mapPoints = [
@@ -23,15 +23,29 @@ const mapPoints = [
   [1.42, 0.0]
 ]
 
-function Map({ entities, perspective }: { entities: EntitiesPayload; perspective: '2d' | '3d' }) {
+function Map({entities, perspective}: { entities: EntitiesPayload; perspective: '2d' | '3d' }) {
   const shape = new Shape(mapPoints.map(([x, y]) => new THREE.Vector2(x, y)))
+  const [animationState, setAnimationState] = React.useState(0)
+  const [currentAnimation, setCurrentAnimation] = React.useState('idle')
 
-  useFrame(({ camera }) => {
-    if (perspective === '2d') {
-      camera.position.lerp(new THREE.Vector3(0, 100, 0), 0.05)
+  useFrame(({camera}) => {
+    if (animationState > 1) return;
+
+    if (currentAnimation === '2d') {
+      camera.position.lerp(new THREE.Vector3(0, 100, 0), 0.1)
+      camera.lookAt(new THREE.Vector3(0, 0, -degToRad(90)))
+    } else if (currentAnimation === '3d') {
+      camera.position.lerp(new THREE.Vector3(0, 100, 100), 0.1)
       camera.lookAt(new THREE.Vector3(0, 0, -degToRad(90)))
     }
+
+    setAnimationState(animationState + 0.01)
   })
+
+  useEffect(() => {
+    setAnimationState(0)
+    setCurrentAnimation(perspective)
+  }, [perspective])
 
   const extrudeSettings = {
     curveSegments: 1,
@@ -44,16 +58,16 @@ function Map({ entities, perspective }: { entities: EntitiesPayload; perspective
 
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshStandardMaterial color='green' />
-      </mesh>
+      {/*<mesh>*/}
+      {/*  <sphereGeometry args={[1, 32, 32]} />*/}
+      {/*  <meshStandardMaterial color='green' />*/}
+      {/*</mesh>*/}
       <mesh rotation={[0, Math.PI / 2, 0]}>
         <mesh rotation={[Math.PI / 2, 0, 0]} position={[-(x / 2), 0, -(y / 2)]}>
-          <extrudeBufferGeometry attach='geometry' args={[shape, extrudeSettings]} />
-          <meshStandardMaterial side={DoubleSide} />
+          <extrudeBufferGeometry attach='geometry' args={[shape, extrudeSettings]}/>
+          <meshStandardMaterial side={DoubleSide}/>
+          <Entities entities={entities}/>
         </mesh>
-        <Entities entities={entities} />
       </mesh>
     </>
   )
