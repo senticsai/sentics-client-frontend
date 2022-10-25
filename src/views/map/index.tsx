c7967c74abc322fccfabbdd13d58dd9879a484fc
Harsh Pandey <harshpan01@gmail.com>
2022-10-25 18:22:17 +0530

    feat: Smooth animation of camera

--- src/views/map/index.tsx
+++ src/views/map/index.tsx
@@ -1,13 +1,11 @@
-import React, {Suspense} from 'react'
-import {Canvas} from '@react-three/fiber'
-import {Bounds, OrbitControls, PerspectiveCamera} from '@react-three/drei'
-import {Button} from '@mui/material'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Bounds, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Button } from '@mui/material'
 import Map from './map'
-import THREE, {Vector3} from "three";
-import * as TWEEN from "@tweenjs/tween.js";
-import animate from "./animate";
+import * as THREE from 'three'
 
-export const MapComponent = ({positions}: { positions: EntitiesPayload }) => {
+export const MapComponent = ({ positions }: { positions: EntitiesPayload }) => {
   const [perspective, setPerspective] = React.useState<'2d' | '3d'>('2d')
   const perspectiveCamera = React.useRef<any>(null)
 
@@ -23,47 +21,19 @@ export const MapComponent = ({positions}: { positions: EntitiesPayload }) => {
     }
 
     setPerspective('2d')
-
-    // *** WITHOUT ANIMATION
-    camera.position.set(0, 100, 0)
-    camera.lookAt(new Vector3(0, 0, 0))
-
-    /**
-     * NOTE: if you use this sometimes scene disappears
-     * camera.rotation.set(0, 0, 0)
-     */
-
-    // TODO achieve the same effect with animation.
-    // *** WITH ANIMATION
-
-    // const position = {x: camera.position.x, y: camera.position.y, z: camera.position.z};
-    // const positionTween = new TWEEN.Tween(position)
-    //   .to({x: 0, y: 100, z: 0})
-    //   .easing(TWEEN.Easing.Linear.None)
-    //   .onUpdate(() => {
-    //     camera.position.set(position.x, position.y, position.z)
-    //     }
-    //   );
-    // positionTween.start();
-
-
   }
-
-  animate((time) => {
-    TWEEN.update(time);
-  });
 
   return (
     <>
       <Canvas>
         <Suspense fallback={null}>
-          <PerspectiveCamera ref={perspectiveCamera} position={[0, 100, 0]} zoom={1} makeDefault/>
-          <OrbitControls enabled={perspective === '3d'} makeDefault/>
+          <PerspectiveCamera ref={perspectiveCamera} position={[0, 100, 0]} zoom={1} makeDefault />
+          <OrbitControls enabled={perspective === '3d'} />
           <Bounds clip>
-            <Map entities={positions}/>
+            <Map entities={positions} perspective={perspective} />
           </Bounds>
         </Suspense>
-        <ambientLight/>
+        <ambientLight />
       </Canvas>
       <Button variant='outlined' onClick={switchPerspective} className='!absolute bottom-16 right-16 bg-black z-10'>
         {perspective}
--- src/views/map/map.tsx
+++ src/views/map/map.tsx
@@ -2,7 +2,9 @@ import { DoubleSide, Shape } from 'three'
 import React from 'react'
 import Entities from './entities'
 import { getSize } from '../../helpers/getSize'
-import * as THREE from "three";
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
+import { degToRad } from 'three/src/math/MathUtils'
 
 // TODO get this from api in the future
 const mapPoints = [
@@ -21,9 +23,15 @@ const mapPoints = [
   [1.42, 0.0]
 ]
 
-function Map({ entities }: { entities: EntitiesPayload }) {
+function Map({ entities, perspective }: { entities: EntitiesPayload; perspective: '2d' | '3d' }) {
   const shape = new Shape(mapPoints.map(([x, y]) => new THREE.Vector2(x, y)))
 
+  useFrame(({ camera }) => {
+    if (perspective === '2d') {
+      camera.position.lerp(new THREE.Vector3(0, 100, 0), 0.05)
+      camera.lookAt(new THREE.Vector3(0, 0, -degToRad(90)))
+    }
+  })
 
   const extrudeSettings = {
     curveSegments: 1,
@@ -36,8 +44,12 @@ function Map({ entities }: { entities: EntitiesPayload }) {
 
   return (
     <>
-      <mesh rotation={[ 0, Math.PI / 2, 0]}>
-        <mesh rotation={[ Math.PI / 2, 0, 0]} position={[-(x / 2), 0,-(y/2)]}>
+      <mesh>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial color='green' />
+      </mesh>
+      <mesh rotation={[0, Math.PI / 2, 0]}>
+        <mesh rotation={[Math.PI / 2, 0, 0]} position={[-(x / 2), 0, -(y / 2)]}>
           <extrudeBufferGeometry attach='geometry' args={[shape, extrudeSettings]} />
           <meshStandardMaterial side={DoubleSide} />
         </mesh>
