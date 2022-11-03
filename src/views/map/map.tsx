8c3771bf15266dd1f9a2cb712731a61bcc0eebc3
Anshuman Singh <77919788+MrSingh2000@users.noreply.github.com>
2022-11-03 12:45:54 +0530

    Smooth rotation (#16)

--- src/views/map/map.tsx
+++ src/views/map/map.tsx
@@ -1,8 +1,9 @@
 import { DoubleSide, Shape } from 'three'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Entities from './entities'
 import { getSize } from '../../helpers/getSize'
 import * as THREE from 'three'
+import * as TWEEN from '@tweenjs/tween.js'
 import { useFrame } from '@react-three/fiber'
 import { degToRad } from 'three/src/math/MathUtils'
 
@@ -23,7 +24,7 @@ const mapPoints = [
   [1.42, 0.0]
 ]
 
-function Map({ entities, perspective, rotation, flip }: { entities: EntitiesPayload; perspective: '2d' | '3d'; rotation: number; flip: {x: number, z: number} }) {
+function Map({ entities, perspective, rotation, flip }: { entities: EntitiesPayload; perspective: '2d' | '3d'; rotation: number; flip: { x: number, z: number } }) {
   const shape = new Shape(mapPoints.map(([x, y]) => new THREE.Vector2(x, y)))
   const wall = shape.clone()
   wall.holes.push(shape)
@@ -32,6 +33,8 @@ function Map({ entities, perspective, rotation, flip }: { entities: EntitiesPayl
   const [currentAnimation, setCurrentAnimation] = React.useState('idle')
 
   useFrame(({ camera }) => {
+    TWEEN.update()
+
     if (animationState > 1) return;
 
     if (currentAnimation === '2d') {
@@ -44,13 +47,14 @@ function Map({ entities, perspective, rotation, flip }: { entities: EntitiesPayl
       setAnimationState(animationState + 0.05)
     }
 
+
   })
 
   useEffect(() => {
     setAnimationState(0)
     setCurrentAnimation(perspective)
   }, [perspective])
-  
+
 
   const extrudeSettings = {
     curveSegments: 1,
@@ -67,13 +71,29 @@ function Map({ entities, perspective, rotation, flip }: { entities: EntitiesPayl
 
   const [x, y] = getSize(mapPoints)
 
+  // TWEENJS for smooth rotation
+  const rotate = { val: rotation - 1 }
+  const [rotateState, setRotateState] = useState(rotate.val)
+  const tween = new TWEEN.Tween(rotate) // Create a new tween that modifies 'rotate'.
+    .to({ val: rotation }, 1000) // Move to 'value === rotation' in 1 second.
+    .easing(TWEEN.Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
+    .onUpdate(() => {
+      setRotateState(rotate.val)
+    })
+
+  useEffect(() => {
+    tween.start()
+  }, [rotation])
+
+
+
   return (
     <>
-      <mesh scale={[flip.x, 1, flip.z]} rotation={[0, (-rotation) * (Math.PI / 2), 0]}>
+      <mesh scale={[flip.x, 1, flip.z]} rotation={[0, (-rotateState) * (Math.PI / 2), 0]}>
         <mesh rotation={[Math.PI / 2, 0, 0]} position={[-(x / 2), 0, -(y / 2)]}>
           <extrudeBufferGeometry attach='geometry' args={[shape, extrudeSettings]} />
           <Entities entities={entities} />
-          <meshStandardMaterial side={DoubleSide} color='#9AA2A4'/>
+          <meshStandardMaterial side={DoubleSide} color='#9AA2A4' />
           <mesh>
             <extrudeBufferGeometry attach='geometry' args={[wall, extrudeSettingsWalls]} />
             <meshStandardMaterial side={DoubleSide} />
